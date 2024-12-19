97f5bb92f49694302c4297024460fb6c116fd693
nanebo13 <d.o.kostenko@student.itc.khai.edu>
2024-12-20 00:17:38 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args)
+    {
+        String BOT_NAME = "Drus";
+        int BIRTH_YEAR = 2023;
+        System.out.println("Hello! My name is " + BOT_NAME);
+        System.out.println("I was created in " + BIRTH_YEAR);
+    }
+
+}
